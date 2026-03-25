cb8ea81a485e6bc74a1652421144b23a3d34ea94
carp-x <liyuxin_2016@163.com>
2026-03-25 16:33:12 +0800

    rename

--- configs/anymal_c/anymal_c.py
+++ configs/anymal_c/anymal_c.py
@@ -7,15 +7,11 @@
 
 The following configuration parameters are available:
 
-* :obj:`ANYMAL_B_CFG`: The ANYmal-B robot with ANYdrives 3.0
 * :obj:`ANYMAL_C_CFG`: The ANYmal-C robot with ANYdrives 3.0
-* :obj:`ANYMAL_D_CFG`: The ANYmal-D robot with ANYdrives 3.0
 
 Reference:
 
-* https://github.com/ANYbotics/anymal_b_simple_description
 * https://github.com/ANYbotics/anymal_c_simple_description
-* https://github.com/ANYbotics/anymal_d_simple_description
 
 """
 
@@ -56,40 +52,6 @@ ANYDRIVE_3_LSTM_ACTUATOR_CFG = ActuatorNetLSTMCfg(
 # Configuration - Articulation.
 ##
 
-ANYMAL_B_CFG = ArticulationCfg(
-    spawn=sim_utils.UsdFileCfg(
-        usd_path=f"{ISAACLAB_NUCLEUS_DIR}/Robots/ANYbotics/ANYmal-B/anymal_b.usd",
-        activate_contact_sensors=True,
-        rigid_props=sim_utils.RigidBodyPropertiesCfg(
-            disable_gravity=False,
-            retain_accelerations=False,
-            linear_damping=0.0,
-            angular_damping=0.0,
-            max_linear_velocity=1000.0,
-            max_angular_velocity=1000.0,
-            max_depenetration_velocity=1.0,
-        ),
-        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
-            enabled_self_collisions=True, solver_position_iteration_count=4, solver_velocity_iteration_count=0
-        ),
-        # collision_props=sim_utils.CollisionPropertiesCfg(contact_offset=0.02, rest_offset=0.0),
-    ),
-    init_state=ArticulationCfg.InitialStateCfg(
-        pos=(0.0, 0.0, 0.6),
-        joint_pos={
-            ".*HAA": 0.0,  # all HAA
-            ".*F_HFE": 0.4,  # both front HFE
-            ".*H_HFE": -0.4,  # both hind HFE
-            ".*F_KFE": -0.8,  # both front KFE
-            ".*H_KFE": 0.8,  # both hind KFE
-        },
-    ),
-    actuators={"legs": ANYDRIVE_3_LSTM_ACTUATOR_CFG},
-    soft_joint_pos_limit_factor=0.95,
-)
-"""Configuration of ANYmal-B robot using actuator-net."""
-
-
 ANYMAL_C_CFG = ArticulationCfg(
     spawn=sim_utils.UsdFileCfg(
         usd_path=f"{ISAACLAB_NUCLEUS_DIR}/Robots/ANYbotics/ANYmal-C/anymal_c.usd",
@@ -123,46 +85,6 @@ ANYMAL_C_CFG = ArticulationCfg(
     soft_joint_pos_limit_factor=0.95,
 )
 """Configuration of ANYmal-C robot using actuator-net."""
-
-
-ANYMAL_D_CFG = ArticulationCfg(
-    spawn=sim_utils.UsdFileCfg(
-        usd_path=f"{ISAACLAB_NUCLEUS_DIR}/Robots/ANYbotics/ANYmal-D/anymal_d.usd",
-        # usd_path=f"{ISAACLAB_NUCLEUS_DIR}/Robots/ANYbotics/ANYmal-D/anymal_d_minimal.usd",
-        activate_contact_sensors=True,
-        rigid_props=sim_utils.RigidBodyPropertiesCfg(
-            disable_gravity=False,
-            retain_accelerations=False,
-            linear_damping=0.0,
-            angular_damping=0.0,
-            max_linear_velocity=1000.0,
-            max_angular_velocity=1000.0,
-            max_depenetration_velocity=1.0,
-        ),
-        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
-            enabled_self_collisions=True, solver_position_iteration_count=4, solver_velocity_iteration_count=0
-        ),
-        # collision_props=sim_utils.CollisionPropertiesCfg(contact_offset=0.02, rest_offset=0.0),
-    ),
-    init_state=ArticulationCfg.InitialStateCfg(
-        pos=(0.0, 0.0, 0.6),
-        joint_pos={
-            ".*HAA": 0.0,  # all HAA
-            ".*F_HFE": 0.4,  # both front HFE
-            ".*H_HFE": -0.4,  # both hind HFE
-            ".*F_KFE": -0.8,  # both front KFE
-            ".*H_KFE": 0.8,  # both hind KFE
-        },
-    ),
-    actuators={"legs": ANYDRIVE_3_LSTM_ACTUATOR_CFG},
-    soft_joint_pos_limit_factor=0.95,
-)
-"""Configuration of ANYmal-D robot using actuator-net.
-
-Note:
-    Since we don't have a publicly available actuator network for ANYmal-D, we use the same network as ANYmal-C.
-    This may impact the sim-to-real transfer performance.
-"""
 
 
 ##
